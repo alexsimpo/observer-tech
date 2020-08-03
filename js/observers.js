5fdc56f53488ba5eac8a838e19c7734c7d12f1bc
Alex Simpson <alxsimpson@gmail.com>
2020-08-03 11:17:42 +1000

    add slide-fade anim

--- js/observers.js
+++ js/observers.js
@@ -1,6 +1,9 @@
 const header = document.querySelector('header');
 const sectionOne = document.querySelector('.home-intro');
 
+const faders = document.querySelectorAll('.fade-in');
+const sliders = document.querySelectorAll('.slide-in');
+
 const sectionOneOptions = {
   rootMargin: "-180px 0px 0px 0px"
 };
@@ -16,3 +19,27 @@ const sectionOneObserver = new IntersectionObserver(function(entries, sectionOne
 }, sectionOneOptions);
 
 sectionOneObserver.observe(sectionOne);
+
+const appearOptions = {
+  threshold: 0.2,
+  rootMargin: "0px 0px -200px 0px"
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting){
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      appearOnScroll.unobserve(entry.target);
+    }
+  })
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+});
